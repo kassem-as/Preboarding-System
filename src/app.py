5872de1cbd0060249bd9d92097c6c84df7e169c4
Kassem Abbas <kassem_abbas6@outlook.com>
2024-03-20 01:28:05 +0100

    latest version

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, DirectoryLoader, PyPDFLoader
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import CohereRerank
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import CohereEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAI, ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -20,32 +20,37 @@ if "start" not in st.session_state:
     embeddings = CohereEmbeddings(model="embed-multilingual-v3.0")
 
 def get_vectorstore_from_documents():
-    loader = TextLoader("documents\Modulhandbuch.txt", encoding='utf-8')
-    document = loader.load()
+    docs = []
+    for doc in [TextLoader("documents\Modulhandbuch.txt", encoding='utf-8'), TextLoader("documents\Prüfungsordnung.txt", encoding='utf-8')]:
+        docs.extend(doc.load())
+    # texts = [doc.page_content for doc in docs]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 900, chunk_overlap = 300)
+    text_chunks = text_splitter.split_documents(docs)
+    
 
+    
     # split the document into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 700, chunk_overlap = 100)
-    text_chunks = text_splitter.split_documents(document)
-    print(len(text_chunks))
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+    # text_chunks = text_splitter.split_documents(docs)
 
     # create a vectorstore from the chunks
     vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
     vector_store.save_local("vector-store")
     return vector_store
-        
+    
 def get_history_aware_retriever():
-    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature = 0)
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
     compressor = CohereRerank(model="rerank-multilingual-v2.0")
 
     compression_retriever = ContextualCompressionRetriever(
-        base_compressor=compressor, base_retriever=st.session_state.vector_store.as_retriever(search_kwargs={"k":10})
+        base_compressor=compressor, base_retriever=st.session_state.vector_store.as_retriever(search_kwargs={"k":20})
     )
 
     prompt = ChatPromptTemplate.from_messages([
         MessagesPlaceholder(variable_name="chat_messages"),
         ("user", "{input}"),
-        ("user", "Given the above conversation, generate a text query emphasizing the last message to look up in the vector store. ONLY return the query")
+        ("system", "In the conversation above, the user and the system discuss various topics. When the user asks follow-up questions, they might use pronouns ('it', 'they', 'them') referring back to previously mentioned subjects, or they might ask new, standalone questions that do not require context from earlier in the conversation. Your task is to either: a) replace pronouns with the specific subject or noun previously discussed for clarity, or b) repeat the question as it is if it's a standalone question not requiring modification. Do not answer the question. Focus solely on clarifying the question or repeating it verbatim.")
     ])
 
     history_aware_retriever = create_history_aware_retriever(llm, compression_retriever, prompt)
@@ -65,7 +70,7 @@ def get_stuff_documents_chain():
 
 def get_response(user_query, retrieval_chain):
     response = retrieval_chain.invoke({
-        "chat_messages": st.session_state.chat_history[-3:],
+        "chat_messages": st.session_state.chat_history,
         "chat_history": st.session_state.chat_history,
         "input": user_query
     })
@@ -74,6 +79,7 @@ def get_response(user_query, retrieval_chain):
 
 # app config
 st.set_page_config(page_title="Computer Science Preboarding System")
+st.title("CS Preboarding System")
 
 
 # session state
